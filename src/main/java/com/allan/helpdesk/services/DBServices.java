3623f21c257e150db70b74141e7cbd854aeee6de
duarte44 <allanantonucci47@gmail.com>
2023-08-01 09:46:42 -0300

    endpoint find all para tecnico

--- src/main/java/com/allan/helpdesk/services/DBServices.java
+++ src/main/java/com/allan/helpdesk/services/DBServices.java
@@ -28,12 +28,15 @@ public class DBServices {
 
         Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "57558540097", "valdir@gmail.com", "123");
         tec1.addPerfil(Perfil.ADMIN);
+        Tecnico tec2 = new Tecnico(null, "Allan D", "55566640097", "allanD@gmail.com", "123");
+        tec2.addPerfil(Perfil.ADMIN);
+
 
         Cliente cli1 = new Cliente(null, "Linus Turvus","07470203028", "Linus@gmail.com", "123");
 
         Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
 
-        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        tecnicoRepository.saveAll(Arrays.asList(tec1, tec2));
         clienteRepository.saveAll(Arrays.asList(cli1));
         chamadoRepository.saveAll(Arrays.asList(c1));
 
--- src/main/java/com/allan/helpdesk/services/TecnicoService.java
+++ src/main/java/com/allan/helpdesk/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.allan.helpdesk.services;
 
 import com.allan.helpdesk.domain.Tecnico;
+import com.allan.helpdesk.domain.dtos.TecnicoDTO;
 import com.allan.helpdesk.repositories.TecnicoRepository;
 import com.allan.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +21,7 @@ public class TecnicoService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id: " + id));
     }
 
+    public List<Tecnico> findAll() {
+        return repository.findAll();
+    }
 }
