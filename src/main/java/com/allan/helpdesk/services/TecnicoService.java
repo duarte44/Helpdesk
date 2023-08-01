cc2644cc9f09bfc00c0e929af558f21e0834bc66
duarte44 <allanantonucci47@gmail.com>
2023-08-01 08:45:25 -0300

    protegendo aplicação contra serialização

---
+++ src/main/java/com/allan/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.allan.helpdesk.services;
+
+import com.allan.helpdesk.domain.Tecnico;
+import com.allan.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
