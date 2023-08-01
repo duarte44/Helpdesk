95b6bfb152e6685f53a07951ff1d934f3ad7e0b8
duarte44 <allanantonucci47@gmail.com>
2023-08-01 09:36:42 -0300

    exceção para objeto não encontrado

---
+++ src/main/java/com/allan/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.allan.helpdesk.resources.exceptions;
+
+import com.allan.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.persistence.Entity;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    private LocalDateTime agora = LocalDateTime.now();
+    private DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm");
+    private String horaFormatada = formatterHora.format(agora);
+
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objctnotFoundException(ObjectNotFoundException ex,
+                                                                HttpServletRequest request){
+
+        StandardError error = new StandardError(horaFormatada, HttpStatus.NOT_FOUND.value(),"Object not found",
+                ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+}
--- src/main/java/com/allan/helpdesk/services/TecnicoService.java
+++ src/main/java/com/allan/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.allan.helpdesk.services;
 
 import com.allan.helpdesk.domain.Tecnico;
 import com.allan.helpdesk.repositories.TecnicoRepository;
+import com.allan.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id: " + id));
     }
 
 }
