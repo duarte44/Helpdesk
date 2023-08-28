00135d8744d7a3b4754b2932050e96492dc1a32e
duarte44 <allanantonucci47@gmail.com>
2023-08-28 17:26:23 -0300

    find All de chamados

--- src/main/java/com/allan/helpdesk/services/ChamadoService.java
+++ src/main/java/com/allan/helpdesk/services/ChamadoService.java
@@ -1,11 +1,16 @@
 package com.allan.helpdesk.services;
 
 import com.allan.helpdesk.domain.Chamado;
+import com.allan.helpdesk.domain.dtos.ChamadoDTO;
 import com.allan.helpdesk.repositories.ChamadoRepository;
 import com.allan.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,4 +25,7 @@ public class ChamadoService {
     }
 
 
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
 }
